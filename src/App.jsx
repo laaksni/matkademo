84f5d0a46f4de336e18caad07a6d4326e809e737
laaksni <niko.laaksonen@hotmail.com>
2024-02-27 13:31:46 +0200

    Kahdeksi komponentiksi jako

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MatkaList from './components/MatkaList'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>MATKAT</h1>
+      <MatkaList />
+    </>
+  )
+}
+
+export default App
